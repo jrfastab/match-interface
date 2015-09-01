e33547853d90420e5f8449bc7286b5dd97ff2fd1
Jeff Shaw <jeffrey.b.shaw@intel.com>
2015-09-01 08:23:04 -0700

    version 0.0.2rc3
    Signed-off-by: Jeff Shaw <jeffrey.b.shaw@intel.com>

--- include/match_version.h
+++ include/match_version.h
@@ -59,7 +59,7 @@
  * formulate a complete version number. The version number will be higher
  * for more recent versions.
  */
-#define MATCH_VER_EXTRA  2
+#define MATCH_VER_EXTRA  3
 
 /*
  * SUFFIX can be
